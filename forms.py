4a2aa9e3367cf17983524e2d0c92568c21af12df
ldiima <dima@minerva.kgi.edu>
2021-03-20 21:42:33 -0700

    🛂 Remove authentication

--- forms.py
+++ forms.py
@@ -1,17 +1,19 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired, Length, InputRequired, EqualTo, ValidationError
+# Not implemented anymore
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField, BooleanField, PasswordField
+# from wtforms.validators import DataRequired, Length, InputRequired, EqualTo, ValidationError
+
+# class LoginForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired()])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     remember_me = BooleanField('Remember Me')
+#     submit = SubmitField('Sign In')
 
 
-class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign Up')
+# class SignupForm(FlaskForm):
+#     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+#     password = PasswordField('Password', validators=[DataRequired()])
+#     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+#     remember_me = BooleanField('Remember Me')
+#     submit = SubmitField('Sign Up')
